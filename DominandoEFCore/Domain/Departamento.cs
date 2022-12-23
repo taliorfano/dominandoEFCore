e574b39a5c814d6a971fd9bf09dbcc4b271060c1
talita.orfano <talita.orfano@contratanet.com.br>
2022-12-23 09:06:15 -0300

    Iniciando projeto para estudos do EntityFramework Core

---
+++ DominandoEFCore/Domain/Departamento.cs
@@ -0,0 +1,10 @@
+﻿namespace DominandoEFCore.Domain
+{
+    public class Departamento
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public bool Ativo { get; set; }
+        public List<Funcionario> Funcionarios { get; set; }
+    }
+}
