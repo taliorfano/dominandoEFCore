47ebf82879b62d66f50e5956f40d9e1753b59dd5
talita.orfano <talita.orfano@contratanet.com.br>
2022-12-26 09:02:05 -0300

    Ensure Created e HealthCheck do banco de dados

---
+++ DominandoEFCore/Data/ApplicationContextCidade.cs
@@ -0,0 +1,19 @@
+﻿using DominandoEFCore.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace DominandoEFCore.Data
+{
+    public class ApplicationContextCidade: DbContext
+    {
+        public DbSet<Cidade> Cidades { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            const string strConnection = "Data source=(localdb)\\mssqllocaldb; Initial Catalog=C002;Integrated Security=true;pooling=true;";
+            optionsBuilder
+                .UseSqlServer(strConnection)
+                .EnableSensitiveDataLogging()
+                .LogTo(Console.WriteLine, Microsoft.Extensions.Logging.LogLevel.Information);
+        }
+    }
+}
--- DominandoEFCore/Program.cs
+++ DominandoEFCore/Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DominandoEFCore.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
+using System;
+
+namespace DominandoEFCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //EnsureCreatedAndDeleted();
+            //GapDoEnsureCreated();
+            HealthCheckBancoDados();
+        }
+
+        static void HealthCheckBancoDados()
+        {
+            using var db = new ApplicationContext();
+            var canConnect = db.Database.CanConnect();
+
+            if (canConnect)
+            {
+                Console.WriteLine("Posso me conectar");
+            }
+            else
+            {
+                Console.WriteLine("Não posso me conectar");
+            }
+        }
+        
+        static void EnsureCreatedAndDeleted()
+        {
+            using var db = new ApplicationContext();
+            //            db.Database.EnsureCreated();
+            db.Database.EnsureDeleted();
+        }
+
+        static void GapDoEnsureCreated()
+        {
+            using var db1 = new ApplicationContext();
+            using var db2 = new ApplicationContextCidade();
+         
+            db1.Database.EnsureCreated();
+            db2.Database.EnsureCreated();
+
+            var databaseCreator = db2.GetService<IRelationalDatabaseCreator>();
+            databaseCreator.CreateTables();
+        }
+    }
+}
+
